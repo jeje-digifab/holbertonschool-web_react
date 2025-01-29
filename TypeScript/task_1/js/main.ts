fbcb32649060c0a487cee2f3f619d858c749ad17
jeje-digifab <j.romand.digifab@gmail.com>
2025-01-29 19:16:14 +0100

    Add task 4

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -1,41 +1,31 @@
-interface Teacher {
-  readonly firstName: string;
-  readonly lastName: string;
-  fullTimeEmployee: boolean;
-  yearsOfExperience?: number;
-  location: string;
-  [key: string]: any;
+interface StudentConstructor {
+  firstName: string;
+  lastName: string;
 }
 
-interface Directors extends Teacher {
-  numberOfReports: number;
+interface StudentClassInterface {
+  workOnHomework(): string;
+  displayName(): string;
 }
 
-interface printTeacherFunction {
-  (firstName: string, lastName: string): string;
+class StudentClass implements StudentClassInterface {
+  firstName: string;
+  lastName: string;
+
+  constructor({ firstName, lastName }: StudentConstructor) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return 'Currently working';
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
 }
 
-const teacher3: Teacher = {
-  firstName: 'John',
-  fullTimeEmployee: false,
-  lastName: 'Doe',
-  location: 'London',
-  contract: false,
-};
-
-const director1: Directors = {
-  firstName: 'John',
-  lastName: 'Doe',
-  location: 'London',
-  fullTimeEmployee: true,
-  numberOfReports: 17,
-};
-
-const printTeacher: printTeacherFunction = (firstName, lastName) => {
-  const firstInitial = firstName.charAt(0).toUpperCase();
-  return `${firstInitial}. ${lastName}`;
-};
-
-console.log(teacher3);
-console.log(director1);
-console.log(printTeacher("John", "Doe"));
+const student = new StudentClass({ firstName: 'John', lastName: 'Doe' });
+console.log(student.workOnHomework());
+console.log(student.displayName());
